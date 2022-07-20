cd411622c553352269fb0bdccfc659b099cdf0c5
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-07-20 12:56:54 -0300

    Update calculadora.service.ts

--- 01-calculadora/src/app/calculadora/services/calculadora.service.ts
+++ 01-calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+    /* Define as constantes utilizadas 
+     para identificar as operações de cálculo */
+     static readonly SOMA: string = '+';
+     static readonly SUBTRACAO: string = '-';
+     static readonly DIVISAO: string = '/';
+     static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+  calcular(num1: number, num2: number, operacao: string): number {
+  	let resultado: number; // armazena o resultado da operação
+
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = num1 + num2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = num1 - num2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = num1 / num2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = num1 * num2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+
+  	return resultado;
+  }
 }
