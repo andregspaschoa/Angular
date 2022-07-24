9cb4baef37fd1ad6572cc529684e11f8b8d716a7
andregspaschoa <paschoa.andre@outlook.com>
2022-07-24 20:26:31 -0300

    Create tarefas-routing.module.ts

---
+++ 02-gerenciador-de-tarefas/src/app/tarefas/tarefas-routing.module.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+import { ListarTarefaComponent } from './listar/';
+import { CadastrarTarefaComponent } from './cadastrar';
+
+export const TarefaRoutes: Routes = [
+    {
+        path: 'tarefas',
+        redirectTo: 'tarefas/listar'
+    },
+    {
+        path: 'tarefas/listar',
+        component: ListarTarefaComponent
+    },
+    {
+        path: 'tarefas/cadastrar',
+        component: CadastrarTarefaComponent
+    }
+];
