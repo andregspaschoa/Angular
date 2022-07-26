cd6ff202d54bdda32e55595d1fb7cdd5113635e1
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-07-26 17:58:24 -0300

    Create jogo-da-velha.component.spec.ts

---
+++ 03-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
+describe('JogoDaVelhaComponent', () => {
+  let component: JogoDaVelhaComponent;
+  let fixture: ComponentFixture<JogoDaVelhaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JogoDaVelhaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(JogoDaVelhaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
