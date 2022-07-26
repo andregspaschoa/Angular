7591dbed94a868e50612d01418699f582d851f5b
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-07-26 10:26:20 -0300

    Update editar-tarefa.component.ts

--- 02-gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
+++ 02-gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-
-import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router'; 
+import { NgForm } from '@angular/forms'; 
 
 import { TarefaService, Tarefa } from '../shared';
 
@@ -15,15 +14,15 @@ export class EditarTarefaComponent implements OnInit {
   @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
   tarefa: Tarefa;
 
-  constructor(
-    private tarefaService: TarefaService,
-    private route: ActivatedRoute,
-    private router: Router) {}
+  constructor(private tarefaService: TarefaService,
+  	private route: ActivatedRoute,
+  	private router: Router) {}
 
-  ngOnInit(): void {
-    let id = +this.route.snapshot.params['id'];
-    this.tarefa = this.tarefaService.buscarPorId(id);
+  ngOnInit() {
+  	let id = +this.route.snapshot.params['id'];
+	this.tarefa = this.tarefaService.buscarPorId(id);
   }
+
   atualizar(): void {
     if (this.formTarefa.form.valid) {
 	    this.tarefaService.atualizar(this.tarefa);
