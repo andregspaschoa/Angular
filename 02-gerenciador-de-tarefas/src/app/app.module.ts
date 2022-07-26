049b92c48bd17782071d67978021725284e1eca1
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-07-26 10:25:54 -0300

    Update app.module.ts

--- 02-gerenciador-de-tarefas/src/app/app.module.ts
+++ 02-gerenciador-de-tarefas/src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TarefasModule } from './tarefas';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     TarefasModule
   ],
