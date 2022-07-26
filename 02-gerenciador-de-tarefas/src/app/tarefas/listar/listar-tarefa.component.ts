106183b32a6df565cb00a0ee96de6ef4c9efce67
andregspaschoa <paschoa.andre@outlook.com>
2022-07-26 07:26:16 -0300

    Update listar-tarefa.component.ts

--- 02-gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ 02-gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -11,20 +11,29 @@ export class ListarTarefaComponent implements OnInit {
 
   tarefas: Tarefa[];
 
-  constructor(private tarefaService: TarefaService) { }
+  constructor(private tarefaService: TarefaService) {}
 
-  ngOnInit(): void {
-    this.tarefas = this.listarTodos();
+  ngOnInit() {
+  	this.tarefas = this.listarTodos();
   }
-  listarTodos(): Tarefa [] {
-    return this.tarefaService.listarTodos();
+
+  listarTodos(): Tarefa[] {
+  	return this.tarefaService.listarTodos();
   }
-  
-  /*remover($event: any, tarefa: Tarefa): void {
+
+  remover($event: any, tarefa: Tarefa): void {
     $event.preventDefault();
-    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?'))
-    this.tarefaService.remover(tarefa.id);
-    this.tarefas = this.tarefaService.listarTodos();
-  }*/
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
 
 }
