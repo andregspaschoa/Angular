25ce4ce2269f611dfa66bf82941a720ff3683141
andregspaschoa <paschoa.andre@outlook.com>
2022-07-26 07:26:39 -0300

    Update tarefas.module.ts

--- 02-gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ 02-gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
-
-
-
+import { EditarTarefaComponent } from './editar';
 
 @NgModule({
   declarations: [  
-    ListarTarefaComponent, CadastrarTarefaComponent
+    ListarTarefaComponent,
+    CadastrarTarefaComponent, 
+    EditarTarefaComponent, 
+    TarefaConcluidaDirective
   ],
   
   imports: [
