9e28c38cefc010b97edbf1a050b3fcd10f655e99
andregspaschoa <paschoa.andre@outlook.com>
2022-07-30 16:01:52 -0300

    jogo-da-velha-final

--- 03-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
+++ 03-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -1,17 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared';
 
 describe('JogoDaVelhaComponent', () => {
   let component: JogoDaVelhaComponent;
   let fixture: ComponentFixture<JogoDaVelhaComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ JogoDaVelhaComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JogoDaVelhaComponent ],
+      providers: [
+        JogoDaVelhaService
+      ]
     })
     .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(JogoDaVelhaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- 03-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ 03-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 import { JogoDaVelhaService } from './shared';
 
-
 @NgModule({
-  declarations: [
-    JogoDaVelhaComponent
-  ],
-
-  exports: [
-    JogoDaVelhaComponent
-  ],
-
   imports: [
     CommonModule
   ],
-
+  declarations: [
+  	JogoDaVelhaComponent
+  ],
+  exports: [
+  	JogoDaVelhaComponent
+  ],
   providers: [
-    JogoDaVelhaService
+  	JogoDaVelhaService
   ]
 })
 export class JogoDaVelhaModule { }
